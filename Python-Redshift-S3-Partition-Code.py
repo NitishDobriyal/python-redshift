5056157054d20a71b8a87cbcffedb1c0916a584d
Nitish Dobriyal <nitish.dobriyal07@gmail.com>
2022-06-18 16:36:26 +0530

    First Commit

---
+++ Python-Redshift-S3-Partition-Code.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import time
+import math
+import numpy as np
+from io import StringIO
+import time,boto3
+import redshift_connector
+
+conn = redshift_connector.connect(
+             host='endpoint',
+                  database='dev',
+                       user='awsuser',
+                            password='yourpassword'
+                              )
+with conn.cursor() as cursor:
+    cursor.execute("select * from test")
+    result=pd.DataFrame(cursor.fetch_dataframe())
+    result.set_index("id", inplace = True)
+
+def upload_s3(df,i):
+    s3 = boto3.client("s3",aws_access_key_id="xyz",aws_secret_access_key="abcd")
+    for id, df_i in  enumerate(np.array_split(df, math.ceil( df.shape[0]/1000000 ))):
+        csv_buf = StringIO()
+        df_i.to_csv(csv_buf, header=True, index=False, sep="|")
+        csv_buf.seek(0)
+        s3.put_object(Bucket="demoavis-useast1", Body=csv_buf.getvalue(), Key='python-name-convention/dataframe-read/'+str(id)+i)
+
+upload_s3(result,"avis.csv")
